9d72114d0f738cfcd10a98a76f6b92c2803b8c45
0150974108@HELI3.LOCAL <0150974108@HELI3.LOCAL>
2019-10-16 12:24:33 +0330

    simple equation

---
+++ equation.py
@@ -0,0 +1,39 @@
+n = input()
+def alamat_gozari(e):
+    for i in range(len(e)):
+        if (e[i][0] != '+' and e[i][0] != '-') and e[i] != '=':
+            e[i] = '+' + e[i]
+    return e
+
+def make_two_sides(e):
+    i = 0
+    e_1 = []
+    e_2 = []
+    while e[i] != '=':
+        e_1.append(e[i])
+        i += 1
+    i += 1
+    while i <= len(e)-1:
+        e_2.append(e[i])
+        i += 1
+    return [e_1,e_2]
+
+# removing x
+e1 = ''
+for i in range(len(n)):
+    if n[i]!='x':
+        e1+=n[i]
+    else:
+        d = i
+
+#/
+        
+e1 = e1.split()
+e1 = alamat_gozari(e1)
+e1 = make_two_sides(e1)
+
+for i in range(len(e1[0])):
+    e1[0][i] = int(e1[0][i])
+for i in range(len(e1[1])):
+    e1[1][i] = int(e1[1][i])
+print(e1)
