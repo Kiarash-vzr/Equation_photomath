dd41c86d2b5fd6ab041b045baeb29ba3f830b25e
amoosai-84 <56339179+amoosai-84@users.noreply.github.com>
2019-11-23 22:05:05 +0330

    sade kardan

--- equation.py
+++ equation.py
@@ -1,14 +1,13 @@
 n = input()
 # defs
-def alamat_gozari(e):
-    for i in range(len(e)):
-        if (e[i][0] != '+' and e[i][0] != '-' and e[i][0] != '*' and e[i][0] != '/')\
-        and e[i] != '=':
-            e[i] = '+' + e[i]
-        #else:
-    
-    return e
-def split_list_by(n, p, q):
+def alamat_gozari(e1):# return e1 with + and -
+    for i in range(len(e1)):
+        if (e1[i][0] != '+' and e1[i][0] != '-' and e1[i][0] != '*' and e1[i][0] != '/' and e1[i][0] != '^')\
+        and e1[i] != '=':
+            e1[i] = '+' + e1[i]
+    return e1
+def split_list_by(n, p, q):#split n by p and q, return the new "n" as "m"\
+    # m = list of n splited by p and q + p and q
     m = []
     l = []
     for i in range(len(n)):
@@ -16,32 +15,40 @@ def split_list_by(n, p, q):
             l.append(n[i])
         if n[i] == p or n[i] == q or i == len(n)-1:
             m.append(l)
+            m.append(n[i])  
             l = []
         
-    return m
-def make_two_sides(e):
+    return m[:-1] 
+def make_two_sides(e1):#make two sides for e1 by '=' , return two sides
+    
     i = 0
     side_1 = []
     side_2 = []
-    while e[i] != '=':
-        side_1.append(e[i])
+    while e1[i] != '=':
+        side_1.append(e1[i])
         i += 1
     i += 1
-    while i <= len(e)-1:
-        side_2.append(e[i])
+    while i <= len(e1)-1:
+        side_2.append(e1[i])
         i += 1
+    #making two sides by '='
     return [side_1,side_2]
 
-def remove_x(n):
-    e1 = ''
-    for i in range(len(n)):
-        if n[i]!='x':
-            e1+=n[i]
-        else:
-            d = i
-    return(e1)
-
-def sum_of_sides(e1):
+'''def remove_and_save_x(e1):# making a new equation without x
+    
+    new_e1 = [e1[0],[]]
+    for j in range(len(e1[0])):
+        if e1[i][j] == '+x': #not adding x
+            pass
+        if e1[i][j] == '+x' and e1[i][j-1] == '*': #not adding zaribe x
+            pass
+        if e1[i][j] == '+x' and e1[i][j-1] == '^': #not adding a^x
+            pass
+        if e1[i][j] == '+x' and e1[i][j-1] == '^': #not adding tavane x
+            pass
+        
+                
+def sum_of_sides(e1):# 
     sum1 = 0
     sum2 = 0
     for i in range(len(e1[0])):
@@ -50,10 +57,12 @@ def sum_of_sides(e1):
     for i in range(len(e1[1])):
         e1[1][i] = int(e1[1][i])
         sum2 += e1[1][i]
-    return [sum1 , sum2]
-
+    return [sum1 , sum2]'''
 
 # defs </>
+
+
+
 
 e1 = n
 e1 = e1.split()
@@ -62,56 +71,64 @@ e1 = make_two_sides(e1)
 
 print(e1)
 print(split_list_by(e1[0], '+', '-'))
-      
-def solve(e1, round_counter):
-    if round_counter == 1:
+print(split_list_by(e1[1], '+', '-'))
+
+def solve_algebra_phrase_layer(e1, layer_counter):# solving without ()
+    if layer_counter == 1:
+        print(e1,'round 1')
         split_e1 = split_list_by(e1, '-', '+')
+        print(split_e1,'round 1','splited')
         #split equation by + and -
         
-        first_layer_answer = 0
+        first_layer_answer = solve_algebra_phrase(split_e1[0], 2)
         for i in range(len(split_e1)):
-            first_layer_answer = first_layer_answer + solve(split_e1[i], 2)
-            
+            if split_e1[i] == '+':
+                first_layer_answer = first_layer_answer + solve_algebra_phrase(split_e1[i+1], 2)
+            if split_e1[i] == '-':
+                first_layer_answer = first_layer_answer - solve_algebra_phrase(split_e1[i+1], 2)
+        #sending parts to the second layer   
         final_answer = first_layer_answer
         print(final_answer)
         return (final_answer)
 
     
-    if round_counter == 2:
+    if layer_counter == 2:
+        print(e1,'round 2')
         split_e1 = split_list_by(e1, '*', '/')
         #split equation by * and /
         
-        second_layer_answer = 0
+        second_layer_answer = solve_algebra_phrase(split_e1[0], 3)
         for i in range(len(split_e1)):
-            second_layer_answer = second_layer_answer * solve(split_e1[i], 3)
+            if split_e1[i] == '*':
+                second_layer_answer = second_layer_answer * solve_algebra_phrase(split_e1[i+1], 3)
+            if split_e1[i] == '/':
+                second_layer_answer = second_layer_answer / solve_algebra_phrase(split_e1[i+1], 3)
 
+        #sending parts to the third layer
+        print (second_layer_answer)
         return (second_layer_answer)
 
     
-    if round_counter == 3:
+    if layer_counter == 3:
+        print(e1,'round 3')
         split_e1 = split_list_by(e1, '^', '^')
         #split equation by ^ and ^
-        third_layer_answer = 0
+        third_layer_answer = solve_algebra_phrase(split_e1[0], 4)
         for i in range(len(split_e1)):
-            third_layer_answer = third_layer_answer ** solve(split_e1[i], 4)
+            if split_e1[i] == '^':
+                third_layer_answer = third_layer_answer ** solve_algebra_phrase(split_e1[i+1], 4)
+        #sending parts to the last layer
         #start solving from the lowewst layer
         return (third_layer_answer)
 
     
-    elif round_counter > 3:
-        e1 = int(e1[0])
+    elif layer_counter > 3:
+        e1 = float(e1[0])
+        print(e1, 'round 4')
         return (e1)
-print(solve(e1[1], 1))
-'''sum_side1 = sum_of_sides(e1)[0]
-sum_side2 = sum_of_sides(e1)[1]
-
-
-if sum_side1 > 0:
-    sum_side2 = sum_side2 - sum_side1
-    sum_side1 = 0
-else:
-    sum_side2 = sum_side1 - sum_side2
-    sum_side1 = 0
-
-print('x = '+str(sum_side2))'''
+def solve_algebra_phrase(e1, layer_counter):# with () using "solve_algebra_phrase_layer"
+    return
+    
+print(solve_algebra_phrase(e1[1], 1))
+e1[1] = solve_algebra_phrase(e1[1], 1)
     
