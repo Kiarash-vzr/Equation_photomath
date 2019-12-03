eb1a325c4d4373b2a82e5a44e5aa6c09ee88ade4
amoosai-84 <56339179+amoosai-84@users.noreply.github.com>
2019-12-04 00:08:13 +0330

    sade kardan

--- equation.py
+++ equation.py
@@ -36,48 +36,7 @@ def make_two_sides(e1):#make two sides for e1 by '=' , return two sides
     #making two sides by '='
     return [side_1,side_2]
 
-'''def bracket_identification(e1):#return e1 as e
-    #e = replacing e1's brackets as lists
-    e = []
-    bracket_exists = False
-    for i in range(len(e1)):#chek if e1 has brackets
-        if e1[i] == '(' and e1[i] == ')':
-            bracket_exists = True
-
-
-
-            
-    if bracket_exists = False:# final layer
-        return (e1)
-    
-    else:# layering
-        i = 0
-        b_counter = 0
-        inside_bracket = False
-        bracket_list = []
-        while i < len(a):
-            if e1[i] != '(' and e1[i] != ')':
-                e.append(e1[i])
-                i += 1
-                
-            elif e1[i] == '(':#beggining of bracket
-                i += 1
-                b_counter += 1
-                inside_bracket = True
-                
-            elif e1[i] == ')':
-                i += 1
-                b_counter -= 1
-                
-            if inside_bracket == True and e1[i] == ')' and b_counter == 0:   
-                #ending of one bracket
-                #adding bracket list to e
-                e.append
-                inside_bracket = False
-                bracket_list = []
-            if b_counter < 0:
-                break'''
-        
+ 
             
         
 '''def remove_and_save_x(e1):# making a new equation without x
@@ -123,19 +82,19 @@ print(split_list_by(e1[1], '+', '-'))
 
 
 
-def solve_polynomial_phrase(e1, layer_counter):# solving without ()
+def solve_polynomial_phrase(e2, layer_counter):# solving without ()
     if layer_counter == 1:
-        print(e1,'round 1')
-        split_e1 = split_list_by(e1, '-', '+')
-        print(split_e1,'round 1','splited')
+        print(e2,'round 1')
+        split_e2 = split_list_by(e2, '-', '+')
+        print(split_e2,'round 1','splited')
         #split equation by + and -
         
-        first_layer_answer = solve_polynomial_phrase(split_e1[0], 2)
-        for i in range(len(split_e1)):
-            if split_e1[i] == '+':
-                first_layer_answer = first_layer_answer + solve_polynomial_phrase(split_e1[i+1], 2)
-            if split_e1[i] == '-':
-                first_layer_answer = first_layer_answer - solve_polynomial_phrase(split_e1[i+1], 2)
+        first_layer_answer = solve_polynomial_phrase(split_e2[0], 2)
+        for i in range(len(split_e2)):
+            if split_e2[i] == '+':
+                first_layer_answer = first_layer_answer + solve_polynomial_phrase(split_e2[i+1], 2)
+            if split_e2[i] == '-':
+                first_layer_answer = first_layer_answer - solve_polynomial_phrase(split_e2[i+1], 2)
         #sending parts to the second layer   
         final_answer = first_layer_answer
         print(final_answer)
@@ -143,16 +102,16 @@ def solve_polynomial_phrase(e1, layer_counter):# solving without ()
 
     
     if layer_counter == 2:
-        print(e1,'round 2')
-        split_e1 = split_list_by(e1, '*', '/')
+        print(e2,'round 2')
+        split_e2 = split_list_by(e2, '*', '/')
         #split equation by * and /
         
-        second_layer_answer = solve_polynomial_phrase(split_e1[0], 3)
-        for i in range(len(split_e1)):
-            if split_e1[i] == '*':
-                second_layer_answer = second_layer_answer * solve_polynomial_phrase(split_e1[i+1], 3)
-            if split_e1[i] == '/':
-                second_layer_answer = second_layer_answer / solve_polynomial_phrase(split_e1[i+1], 3)
+        second_layer_answer = solve_polynomial_phrase(split_e2[0], 3)
+        for i in range(len(split_e2)):
+            if split_e2[i] == '*':
+                second_layer_answer = second_layer_answer * solve_polynomial_phrase(split_e2[i+1], 3)
+            if split_e2[i] == '/':
+                second_layer_answer = second_layer_answer / solve_polynomial_phrase(split_e2[i+1], 3)
 
         #sending parts to the third layer
         print (second_layer_answer)
@@ -160,22 +119,86 @@ def solve_polynomial_phrase(e1, layer_counter):# solving without ()
 
     
     if layer_counter == 3:
-        print(e1,'round 3')
-        split_e1 = split_list_by(e1, '^', '^')
+        print(e2,'round 3')
+        split_e2 = split_list_by(e2, '^', '^')
         #split equation by ^ and ^
-        third_layer_answer = solve_polynomial_phrase(split_e1[0], 4)
-        for i in range(len(split_e1)):
-            if split_e1[i] == '^':
-                third_layer_answer = third_layer_answer ** solve_polynomial_phrase(split_e1[i+1], 4)
+        third_layer_answer = solve_polynomial_phrase(split_e2[0], 4)
+        for i in range(len(split_e2)):
+            if split_e2[i] == '^':
+                third_layer_answer = third_layer_answer ** solve_polynomial_phrase(split_e2[i+1], 4)
         #sending parts to the last layer
         #start solving from the lowewst layer
         return (third_layer_answer)
 
     
     elif layer_counter > 3:
-        e1 = float(e1[0])
-        print(e1, 'round 4')
-        return (e1)
+        e2 = float(e2[0])
+        print(e2, 'round 4')
+        return (e2)
+
+
+def bracket_identification(e1):#return e1 as e
+    #e = replacing e1's brackets as their answers
+    
+    print(e1, "     = e1")
+    e = []
+    bracket_exists = False
+    for i in range(len(e1)):#chek if e1 has brackets
+        if e1[i] == '(' or e1[i] == ')':
+            bracket_exists = True
+
+    print(e1, "1")        
+    if bracket_exists == False:# final layer
+        return (solve_polynomial_phrase(e1, 1))
+    print(e1 , 2)
+    
+    if bracket_exists == True:# layering
+        print(e1)
+        i = 0
+        b_counter = 0
+        inside_bracket = False
+        bracket_list = []
+        e = []
+        while i < len(e1):
+            print('//////////////////////////')
+            print(e1, i)
+            print(b_counter, "b_counter")
+            print(inside_bracket, "inside_bracket")
+            print(bracket_list, "bracket_list")
+            print(e , "e")
+
+            if e1[i] != '(' and e1[i] != ')':
+                if inside_bracket == True:
+                    bracket_list.append (e1[i])
+                else:
+                    e.append(e1[i])
+                i += 1
+          
+            elif e1[i] == '(':#beggining of bracket
+                if inside_bracket == True:
+                    bracket_list.append (e1[i])
+                    
+                b_counter += 1
+                inside_bracket = True
+                i += 1
+                
+            elif e1[i] == ')':
+                if inside_bracket == True:
+                    bracket_list.append (e1[i])
+                i += 1
+                b_counter -= 1
+                
+            if inside_bracket == True and e1[i-1] == ')' and b_counter == 0:
+                print("!!!!!!!!!!!!!!!")
+                #ending of one bracket
+                #adding bracket list to e
+                #caling the function for the bracket
+                bracket_list = bracket_list[:-1]
+                e.append (bracket_identification(bracket_list))
+                inside_bracket = False
+                bracket_list = []
+                
+    return(solve_polynomial_phrase(e, 1))
     
 '''def solve_algebra_phrase(e1, layer_counter):# with () using "solve_polynomial_phrase"
     e = []
@@ -191,6 +214,5 @@ def solve_polynomial_phrase(e1, layer_counter):# solving without ()
             
     return'''
     
-print(solve_polynomial_phrase(e1[1], 1))
-e1[1] = solve_polynomial_phrase(e1[1], 1)
+print(bracket_identification(e1[1]))
     
